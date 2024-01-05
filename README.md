0a5e5eb2d8a410ab6ce047c8cc7e30ed5700a893
JR-Vickers <jarrett_vickers@yahoo.com>
2024-01-04 23:04:47 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+# Provably Random Raffle Contracts
+
+## About
+
+This code is to create a provably random smart contract lottery.
+
+## What do we want it to do?
+
+1. Users can enter by paying for a ticket
+   1. The ticket fees are going to the winner during the draw
+2. After X period of time, the lottery will automatically draw a winner
+   1. And this will be done programmatically
+3. Use Chainlink VRF & Chainlink Automation
+   1. Chainlink VRF -> Randomness
+   2. Chainlink Automation -> Time based trigger
+
+---
+
 ## Foundry
 
 **Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
---
+++ src/Raffle.sol
@@ -0,0 +1,102 @@
+/**
+ * Layout of Contract:
+ * version
+ * imports
+ * errors
+ * interfaces, libraries, contracts
+ * Type declarations
+ * State variables
+ * Events
+ * Modifiers
+ * Functions
+ * 
+ * Layout of Functions:
+ * constructor
+ * receive function (if exists)
+ * fallback function (if exists)
+ * external
+ * public
+ * internal
+ * private
+ * view & pure functions
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+/**
+ * @title A sample Raffle contract
+ * @author Jarrett Vickers
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+    error Raffle__NotEnoughEthSent();
+
+    /** State Variables */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /** Events */
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit){
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
+    }
+    
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH to enter raffle");
+        if(msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
+
+    // 1. Get a random number
+    // 2. use the random number to pick a player
+    // 3. Be automatically called
+    function pickWinner() external {
+        // check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval){
+            revert();
+        }
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /** Getter Function */
+
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
+}
